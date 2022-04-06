e10fbcdda4c3831a89baa92ae719cd4cb2ce4233
Yongwook Jung <ywjung99@naver.com>
2022-04-06 14:16:26 +0900

    Update 001_create_approvals.rb

--- db/migrate/001_create_approvals.rb
+++ db/migrate/001_create_approvals.rb
@@ -1,4 +1,4 @@
-class CreateApprovals < ActiveRecord::Migration
+class CreateApprovals < ActiveRecord::Migration[5.1]
   def change
     create_table :approvals do |t|
       t.references :changeset, :null => false
